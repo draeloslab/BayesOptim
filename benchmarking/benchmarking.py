6c6729a3358a95b64bce75652b83663e38fd79ec
Dominic <dbonasse@umich.edu>
2026-01-28 16:36:16 -0500

    Changed parameter file path from parameters_all to parameters

--- benchmarking/benchmarking.py
+++ benchmarking/benchmarking.py
@@ -2,13 +2,13 @@ import sys
 import time
 import yaml
 from datetime import datetime
-from plot import plot_correct_prediction, plot_peak_value, plot_run_time, plot_acqf, plot_mse#, plot_tuningcurves_eval
-from utils.save_results import save_results
 import os
 os.environ['JAX_PLATFORM_NAME'] = 'cpu'
 import jax
 import jax.numpy as np
 
+from plot import plot_correct_prediction, plot_peak_value, plot_run_time, plot_acqf, plot_mse#, plot_tuningcurves_eval
+from utils.save_results import save_results
 from model.bayesopt_sampling import bayesopt_sampling 
 from model.kernel import kernel_rbf
 from model.kernel import kernel_rbf_periodic
@@ -19,7 +19,7 @@ from model.sim_config import Config
 start = time.time()
 
 parameter_file = "parameters_indep.yml"
-param_file_path = os.path.join('parameters_all', parameter_file)
+param_file_path = os.path.join('parameters', parameter_file)
 with open(param_file_path, 'r') as file:
     parameters = yaml.safe_load(file)
 
